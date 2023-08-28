0473e0d72ae675c716fe968bdd2c5c942dfe2408
gunasantosh <gunasantosh999@gmail.com>
2023-08-28 10:43:31 +0530

    v1

--- website/views.py
+++ website/views.py
@@ -1,12 +1,14 @@
 from flask import Blueprint
-from flask import render_template,jsonify,request
-from flask_login import login_required,current_user
+from flask import render_template, jsonify, request
+from flask_login import login_required, current_user
 from .models import User
 import mysql.connector
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
-con = mysql.connector.connect(host='localhost', user='root', password='mypassword', database='dapp')
+con = mysql.connector.connect(
+    host="localhost", user="root", password="mypassword", database="dapp"
+)
 cursor = con.cursor(buffered=True)
 
 
@@ -20,14 +22,15 @@ def home():
 @login_required
 def dashboard():
     role = current_user.role
-    user_list = User.query.filter_by(role='user').all() 
-    return render_template('dashboard.html', role=role, user_list=user_list)
+    user_list = User.query.filter_by(role="user").all()
+    return render_template("dashboard.html", role=role, user_list=user_list)
+
 
 # @views.route('/save', methods=['POST'])
 # def save():
 #     image_data = request.json.get('image_data')
 #     if image_data:
-       
+
 #         query = "INSERT INTO captured_image (image) VALUES ('{}')".format(image_data)
 #         cursor.execute(query)
 #         con.commit()
@@ -35,7 +38,8 @@ def dashboard():
 #     else:
 #         return jsonify({'status': 'error'})
 
-@views.route('/process_form', methods=['POST'])
+
+@views.route("/process_form", methods=["POST"])
 @login_required
 def process_form():
     meter_reading = request.form.get("reading")
@@ -43,17 +47,10 @@ def process_form():
     username = request.form.get("selectedUser")
     print(request.form.get("imgurl"))
     if meter_reading:
-        query = "INSERT INTO captured_image (meter_reading, image, username) VALUES ('{}','{}','{}')".format(meter_reading, image, username)
+        query = "INSERT INTO captured_image (meter_reading, image, username) VALUES ('{}','{}','{}')".format(
+            meter_reading, image, username
+        )
         cursor.execute(query)
         con.commit()
-        return render_template('dashboard.html', msg = "Meter reading Submitted !!")
-    return render_template('dashboard.html')
-
-
-
-
-
-
-
-
-
+        return render_template("dashboard.html", msg="Meter reading Submitted !!")
+    return render_template("dashboard.html")
