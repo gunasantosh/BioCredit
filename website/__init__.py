cb7fbab24e293bf39bdb32a9cdc6f330815966ba
gunasantosh <gunasantosh999@gmail.com>
2023-08-29 21:34:29 +0530

    v2.2

--- website/__init__.py
+++ website/__init__.py
@@ -1,24 +1,25 @@
 from flask import Flask
 from os import path
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
+    app = Flask("__main__")
+    app.config["SECRET_KEY"] = "gt"
+    app.template_folder = "website/templates"
+    app.static_folder = "website/static"
 
-    app = Flask('__main__')
-    app.config['SECRET_KEY'] = 'gt'
-    app.template_folder = 'website/templates'
-    app.static_folder = 'website/static'
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/dapp'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:mypassword@localhost/dapp"
 
     db.init_app(app)
 
     from website.views import views
     from website.auth import auth
+
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
@@ -27,7 +28,7 @@ def create_app():
     from website.models import User
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -36,8 +37,9 @@ def create_app():
 
     return app
 
+
 def create_database(app):
-    if not path.exists('website/'+DB_NAME):
+    if not path.exists("website/" + DB_NAME):
         with app.app_context():
             db.create_all()
         print("Created Database!")
--- website/views.py
+++ website/views.py
@@ -11,7 +11,7 @@ from geopy.distance import geodesic
 views = Blueprint("views", __name__)
 
 con = mysql.connector.connect(
-    host="localhost", user="root", password="", database="dapp"
+    host="localhost", user="root", password="mypassword", database="dapp"
 )
 cursor = con.cursor(buffered=True)
 
@@ -22,15 +22,15 @@ def home():
     return render_template("index.html")
 
 
-@views.route("/dashboard",methods=['GET','POST'])
+@views.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
-    msg=None
-    if(request.method=='GET'):
-        msg=request.args.get('msg')
+    msg = None
+    if request.method == "GET":
+        msg = request.args.get("msg")
     role = current_user.role
     user_list = User.query.filter_by(role="user").all()
-    return render_template("dashboard.html", role=role, user_list=user_list,msg=msg)
+    return render_template("dashboard.html", role=role, user_list=user_list, msg=msg)
 
 
 # @views.route('/save', methods=['POST'])
@@ -51,41 +51,57 @@ def dashboard():
 def process_form():
     meter_reading = request.json["reading"]
     image = request.json["imgurl"]
-    latitude=request.json['latitude']
-    longitude=request.json['longitude']
-    accuracy=request.json['accuracy']
-    current_location=(latitude,longitude)
-    user_latitude=current_user.latitude
-    user_longitude=current_user.longitude
-    user_location=(user_latitude,user_longitude)
-    if(current_user.role=='superuser'):
+    latitude = request.json["latitude"]
+    longitude = request.json["longitude"]
+    accuracy = request.json["accuracy"]
+    current_location = (latitude, longitude)
+    user_latitude = current_user.latitude
+    user_longitude = current_user.longitude
+    user_location = (user_latitude, user_longitude)
+    if current_user.role == "superuser":
         username = request.json["selectedUser"]
     else:
         username = current_user.username
 
     query = "SELECT * FROM user WHERE username='{}'".format(username)
     cursor.execute(query)
-    res=cursor.fetchall()
+    res = cursor.fetchall()
     print(res)
-    user_latitude=res[0][5]
-    user_longitude=res[0][6]
-    user_accuracy=res[0][7]
-    user_location=(user_latitude,user_longitude)
-    
+    user_latitude = res[0][5]
+    user_longitude = res[0][6]
+    user_accuracy = res[0][7]
+    user_location = (user_latitude, user_longitude)
+
     print(current_location)
     print(user_location)
-    distance=geodesic(current_location,user_location)
+    distance = geodesic(current_location, user_location)
     print(distance)
-    if(distance<=(float(accuracy)+float(user_accuracy)+200)):
-        today=datetime.today().strftime('%Y-%m-%d')
+    if distance <= (float(accuracy) + float(user_accuracy) + 200):
+        today = datetime.today().strftime("%Y-%m-%d")
         try:
             query = "INSERT INTO readings (meter_reading,date, image, username) VALUES ('{}','{}','{}','{}')".format(
-                meter_reading,today, image, username
+                meter_reading, today, image, username
             )
             cursor.execute(query)
             con.commit()
-            return jsonify({"statusCode":200,"msg":"Meter reading Submitted Sucessfully.!!"})
+            return jsonify(
+                {"statusCode": 200, "msg": "Meter reading Submitted Sucessfully.!!"}
+            )
         except errors.IntegrityError as e:
-            return jsonify({"statusCode":999,"msg":"Meter reading Submission Failed,\nAs Todays reading of {}'s Meter is already Submitted.!!".format(username)})
+            return jsonify(
+                {
+                    "statusCode": 999,
+                    "msg": "Meter reading Submission Failed,\nAs Todays reading of {}'s Meter is already Submitted.!!".format(
+                        username
+                    ),
+                }
+            )
     else:
-        return jsonify({"statusCode":999,"msg":"Please Submit the Form While Standing near the {}'s Meter.!!".format(username)})
+        return jsonify(
+            {
+                "statusCode": 999,
+                "msg": "Please Submit the Form While Standing near the {}'s Meter.!!".format(
+                    username
+                ),
+            }
+        )
